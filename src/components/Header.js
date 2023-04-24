e732c3d71fc525b9663bd0c7c38b4c3f68980d90
Aidan Mckenzie <adn.mck@gmail.com>
2023-04-24 15:45:24 +0100

    fixed broken image urls

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { BiExit } from 'react-icons/bi';
 const Header = ({ score, bestScore, gameOver, handleGameOver }) => {
   return (
     <header>
-      <img src="../images/logo.png" alt="" />
+      <img src="./images/logo.png" alt="" />
       <h1>Memory Game</h1>
       {!gameOver && <button onClick={() => handleGameOver()}><BiExit title="Return to title screen"/></button>}
       <ul>
