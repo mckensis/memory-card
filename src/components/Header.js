d3df97b967a9ab566bb5fbbc423b0d1617514d92
Aidan Mckenzie <adn.mck@gmail.com>
2023-04-24 15:22:23 +0100

    updated header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import { BiExit } from 'react-icons/bi';
+
 const Header = ({ score, bestScore, gameOver, handleGameOver }) => {
   return (
     <header>
       <img src="../images/logo.png" alt="" />
       <h1>Memory Game</h1>
-      {!gameOver && <button onClick={() => handleGameOver()}>Return to Title</button>}
+      {!gameOver && <button onClick={() => handleGameOver()}><BiExit title="Return to title screen"/></button>}
       <ul>
         <li>Best Score: {bestScore}</li>
         <li>Current Score: {score}</li>
